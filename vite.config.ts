48473c69fc03d8f56c7dd353857c18be1937f537
Nimrod Shalit <nishalit/2microsoft.com>
2025-11-16 14:44:33 +0200

    Changed repository name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shalit-family-task-app/",
+  base: "/Shalit-Familt-task-app/",
   plugins: [react()],
   build: {
     rollupOptions: {
