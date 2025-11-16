9fdd95ecc8541e51b158971f6120025f92beba9b
Nimrod Shalit <nishalit/2microsoft.com>
2025-11-16 11:52:32 +0200

    changes to make build successful

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/shalit-family-task-app/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@google/genai']
+    }
+  }
 })
